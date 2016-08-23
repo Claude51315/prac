ae2b3a165d7e679f64571b1a8acc96077f4e56f4
claude_yen <claude_yen@phison.com>
2016-08-23 09:22:49 +0800

    plot temperature

--- R_TSP/TSP.R
+++ R_TSP/TSP.R
@@ -29,6 +29,9 @@ opt_path = path;
 # SA
 temperature = T_init ;
 nIter = city_number *10; 
+loop_count = 1 ; 
+temperature_stat = vector(mode = "numeric" , length = ceiling ((log(T_min) - log(T_init)) / log(r)));
+
 while(temperature > T_min){
   
   for(i in c(1:nIter)){
@@ -47,15 +50,18 @@ while(temperature > T_min){
   }
   temperature = temperature * r ;
   print(paste("temperature = " , temperature , ", optimal = ", opt_len , sep = " "));
+  temperature_stat[loop_count] = temperature ; 
+  loop_count = loop_count + 1 ; 
 }
 
 # plot result 
 cities_coordinate = cities_coordinate / 111000; 
 X11()
+par(mfrow = c(1 , 2));
 plot(cities_coordinate[,2] , cities_coordinate[,1], col = 'red' , main = paste("total distance =", opt_len, sep = " ") , 
      xlab = "longitude" , ylab = "latitude")
 lines(cities_coordinate[opt_path, 2], cities_coordinate[opt_path,1] , col = 'blue')
 ## connecting first and last cities 
 lines(cities_coordinate[opt_path[c(1,city_number)] ,2],cities_coordinate[opt_path[c(1,city_number)] ,1 ], col = 'blue')
 
-
+plot(temperature_stat , main = "temperature" , type = 'l')
