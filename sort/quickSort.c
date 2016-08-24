3e8c2569bbed030bab6243f5e9a48d8cb4135a25
claude51315 <claude51315@gmail.com>
2016-08-24 11:20:54 +0800

    add quickSort.c

---
+++ sort/quickSort.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define N 100
+
+void quick_sort(int *data, int start, int end);
+void print(int *a, int n);
+int a[N]; 
+int main(){
+	srand(time(NULL));
+	for(int i = 0 ; i < N; i++)
+		a[i] = rand()%(N*10);
+	print(a ,N);
+	quick_sort(a , 0 , N-1); 
+	print(a ,N);
+	return 0 ; 
+}
+void quick_sort(int *data, int start , int end )
+{
+	if(start >= end ) return ;
+	int pivot = data[start]; 
+	int l , r, tmp;
+	// partition
+	l = start ; 
+	r = end; 
+	while(l < r)
+	{
+		while(l < end && data[l] <= pivot) l++; 
+		while(r > start && data[r] > pivot) r--; 
+		if(l <  r){ 
+		// swap 
+		tmp = data[l];
+		data[l] = data[r] ; 
+		data[r] = tmp;
+		}
+	}
+	data[start] = data[r] ; 
+	data[r] = pivot ;
+
+	quick_sort(data , start , r-1 ) ; 
+	quick_sort(data , r+1 , end);
+}
+void print(int *a, int n)
+{
+	for(int i = 0 ; i < n ;i++)
+		printf("%d ", a[i] );
+	printf("\n" );
+}
