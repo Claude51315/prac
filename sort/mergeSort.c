e41cbb6637579465f48b06a912b9033b09fec5e5
claude51315 <claude51315@gmail.com>
2016-08-25 11:39:49 +0800

    fix coding style

--- sort/mergeSort.c
+++ sort/mergeSort.c
@@ -4,59 +4,59 @@
 
 #define N 100
 
-void merge_sort(int *a, int n);
+void merge_sort(int *data, int n);
 void merge(int *result, int *L, int l_len, int *R, int r_len);
-void print(int *a, int n);
+void print(int *data, int n);
 int main(){
 	
-	int a[N]; 
+	int data[N]; 
 	srand(time(NULL));
 	for(int i = 0 ; i < N; i++)
-		a[i] = rand()%(N*10);
-	print(a ,N);
-	merge_sort(a, N); 
-	print(a ,N);
+		data[i] = rand()%(N*10);
+	print(data ,N);
+	merge_sort(data, N); 
+	print(data ,N);
 	return 0 ; 
 }
-void merge_sort(int *a, int n)
+void merge_sort(int *data, int n)
 {
 	if(n < 2)return ; 
 	int mid, i, *L, *R; 
-	mid  = n /2 ; 
-	L =(int*) malloc(sizeof(int) *mid);
-	R =(int*) malloc(sizeof(int) *(n - mid));
-	for(i = 0; i < mid ; i ++)
-		L[i] = a[i]; 
-	for(i = mid ; i < n ; i ++)
-		R[i-mid] = a[i] ; 
-	merge_sort(L , mid); 
-	merge_sort(R , n-mid);
-	merge(a , L , mid, R , n-mid);
+	mid  = n / 2; 
+	L =(int*) malloc(sizeof(int) * mid);
+	R =(int*) malloc(sizeof(int) * (n - mid));
+	for(i = 0; i < mid; i++)
+		L[i] = data[i]; 
+	for(i = mid ; i < n; i++)
+		R[i-mid] = data[i] ; 
+	merge_sort(L, mid); 
+	merge_sort(R, n-mid);
+	merge(data, L, mid, R, n-mid);
 	free(L);
 	free(R); 
 }
-void merge(int *result, int *L, int l_len, int *R, int r_len)
+void merge(int *data, int *L, int l_len, int *R, int r_len)
 {
-	int i, j , k; 
+	int i, j, k; 
 	/*
 		i : left array index 
 		j : right array index 
-		k :  merge array index 
+		k : merge array index 
 	*/
 	i = 0 ; j = 0 ; k = 0; 
 	
 	while(i < l_len && j < r_len)
 	{
-		if(L[i] < R[j]) result[k++] = L[i++];
-		else result[k++] = R[j++];
+		if(L[i] < R[j]) data[k++] = L[i++];
+		else data[k++] = R[j++];
 	}
-	while(i < l_len) result[k++] = L[i++]; 
-	while(j < r_len) result[k++] = R[j++]; 
+	while(i < l_len) data[k++] = L[i++]; 
+	while(j < r_len) data[k++] = R[j++]; 
 	return ; 
 }
-void print(int *a, int n)
+void print(int *data, int n)
 {
-	for(int i = 0 ; i < n ;i++)
-		printf("%d ", a[i] );
-	printf("\n" );
+	for(int i = 0; i < n; i++)
+		printf("%d ", data[i] );
+	printf("\n");
 }
--- sort/quickSort.c
+++ sort/quickSort.c
@@ -6,22 +6,22 @@
 
 void quick_sort(int *data, int start, int end);
 void print(int *a, int n);
-int a[N]; 
 int main(){
+	int a[N]; 
 	srand(time(NULL));
-	for(int i = 0 ; i < N; i++)
+	for(int i = 0; i < N; i++)
 		a[i] = rand()%(N*10);
-	print(a ,N);
-	quick_sort(a , 0 , N-1); 
-	print(a ,N);
-	return 0 ; 
+	print(a, N);
+	quick_sort(a, 0, N-1); 
+	print(a, N);
+	return 0; 
 }
-void quick_sort(int *data, int start , int end )
+void quick_sort(int *data, int start, int end )
 {
-	if(start >= end ) return ;
+	if(start >= end ) return;
 	int pivot = data[start]; 
-	int l , r, tmp;
-	// partition
+	int l, r, tmp;
+	/* partition */
 	l = start ; 
 	r = end; 
 	while(l < r)
@@ -29,21 +29,20 @@ void quick_sort(int *data, int start , int end )
 		while(l < end && data[l] <= pivot) l++; 
 		while(r > start && data[r] > pivot) r--; 
 		if(l <  r){ 
-		// swap 
-		tmp = data[l];
-		data[l] = data[r] ; 
-		data[r] = tmp;
+			tmp = data[l];
+			data[l] = data[r] ; 
+			data[r] = tmp;
 		}
 	}
 	data[start] = data[r] ; 
 	data[r] = pivot ;
-
+	/* divide and conquer */
 	quick_sort(data , start , r-1 ) ; 
 	quick_sort(data , r+1 , end);
 }
-void print(int *a, int n)
+void print(int *data, int n)
 {
-	for(int i = 0 ; i < n ;i++)
-		printf("%d ", a[i] );
-	printf("\n" );
+	for(int i = 0; i < n ;i++)
+		printf("%d ", data[i]);
+	printf("\n");
 }
