f07a2030617ae2a5851e626e7ef434df0540f4ff
claude51315 <claude51315@gmail.com>
2016-09-04 21:26:35 +0800

    update main.c

--- sort/src/main.c
+++ sort/src/main.c
@@ -1,15 +1,21 @@
 #include "list.h"
-#define N 100
+#define N 10
 int main(void)
 {
-    list_t *the_list;
+    list_t *the_list, *the_list2, *merge_list;
     the_list = init_list();
+    the_list2 = init_list();   
     int i ;
-    for(i = 0; i < N ; i ++)
-        insert(the_list, N-i);
-    print(the_list);
-    the_list = bubble_sort(the_list);
-    print(the_list);
-    printf("\n");
+    for(i = 0; i < N; i++)
+    {
+        list_insert(the_list, N - i);
+        list_insert(the_list2, N - i);
+    }
+    merge_list = merge_sort(the_list);
+    bubble_sort(the_list2);
+    printf("merge sort:\n");
+    print(merge_list);
+    printf("bubble sort:\n");
+    print(the_list2);
     return 0 ;
 }
