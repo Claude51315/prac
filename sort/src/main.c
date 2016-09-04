a48f4351aeb2dc1bc0d9f8dffe0536479f45d91d
claude51315 <claude51315@gmail.com>
2016-09-04 15:28:18 +0800

    add main.c

--- sort/src/main.c
+++ sort/src/main.c
@@ -5,8 +5,11 @@ int main(void)
     list_t *the_list;
     the_list = init_list();
     int i ;
-    for(i = 0 ; i < N ; i ++)
-        insert(the_list, i);
+    for(i = 0; i < N ; i ++)
+        insert(the_list, N-i);
     print(the_list);
+    the_list = bubble_sort(the_list);
+    print(the_list);
+    printf("\n");
     return 0 ;
 }
