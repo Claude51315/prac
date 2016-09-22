880c0ab52738035bd077779af6f4ff4f4f1a2212
claude51315 <claude51315@gmail.com>
2016-09-22 10:44:28 +0800

    complete Tutorial Chap. 2

--- dart/angular2_tour_of_heros/lib/app_component.dart
+++ dart/angular2_tour_of_heros/lib/app_component.dart
@@ -1,5 +1,25 @@
 import 'package:angular2/core.dart';
 @Component(
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>')
-class AppComponent {}
+    template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id:</label>{{hero.id}}</div>
+    <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+        
+    </div>
+    '''
+    )
+
+class AppComponent {
+    String title = 'Tour of Heros';
+    Hero hero = new Hero(1, 'windstorm');
+}
+class Hero {
+    final int id;
+    String name;
+
+    Hero(this.id, this.name);
+}
