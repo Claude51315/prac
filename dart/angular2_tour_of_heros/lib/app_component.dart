8bde96a6de01fd890079e5f626016a7ab1bd41bd
claude51315 <claude51315@gmail.com>
2016-09-24 20:25:17 +0800

    add basic routing

--- dart/angular2_tour_of_heros/lib/app_component.dart
+++ dart/angular2_tour_of_heros/lib/app_component.dart
@@ -1,91 +1,25 @@
-import 'dart:async';
 import 'package:angular2/core.dart';
-import 'hero.dart';
-import 'hero_detail_component.dart';
+import 'package:angular2/router.dart';
 import 'hero_service.dart';
+import 'heroes_component.dart';
+
 @Component(
     selector: 'my-app',
     template: '''
         <h1>{{title}}</h1>
-        <h2>My Heros</h2>
-        <ul class="heroes" >
-            <li *ngFor="let hero of heroes" 
-                [class.selected]="hero == selectedHero"
-                (click)="onSelect(hero)">
-                <span class="badge"> {{hero.id}}</span>{{hero.name}}
-            </li>
-        </ul>
-        <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+        <a [routerLink]="['Heroes']">Heroes</a>
+        <router-outlet></router-outlet>
     ''',
-    styles: const [
-    '''
-        .selected {
-            background-color: #CFD8DC !important;
-            color: white;
-        }
-        .heroes {
-            margin: 0 0 2em 0;
-            list-style-type:none;
-            padding: 0;
-            width: 10em;
-        }
-        .heroes li {
-            cursor: pointer;
-            position: relative;
-            left: 0;
-            background-color: #EEE;
-            margin: .5em;
-            padding: .3em 0em;
-            height: 1.6em;
-            border-radius: 4px;
-        }
-        .heroes li.selected:hover {
-            color: white;
-        }
-        .heroes li:hover {
-            color: #607D8B;
-            background-color: #EEE;
-            left: .1em;
-        }
-        .heroes .text {
-            position: relative;
-            top: -3px;
-        }
-        .heroes .badge {
-            display: inline-block;
-            font-size: small;
-            color: white;
-            padding: 0.8em 0.7em 0 em 0.7em;
-            background-color: #607D8B;
-            line-hieght: 1em;
-            position: relative;
-            left: -1px;
-            top: -4px;
-            height: 1.8em;
-            margin-right: .8em;
-            border-radius: 4px 0px 0ox 4px;
-        }
-    '''
-    ],
-    directives: const [HeroDetailComponent],
-    providers: const [HeroService]
+    directives: const [ROUTER_DIRECTIVES],
+    providers: const [HeroService, ROUTER_PROVIDERS]
+
 )
 
-class AppComponent implements OnInit {
-    String title = 'Tour of Heros';
-    List<Hero> heroes ;
-    Hero selectedHero;
-    final HeroService _heroService;
-    AppComponent(this._heroService);
-    
-    Future<NULL> getHeroes() async{
-        heroes = await _heroService.getHeroes();
-    }
-    void ngOnInit(){
-        getHeroes();
-    }
-    onSelect(Hero hero){
-        selectedHero = hero;
-    }
+@RouteConfig(
+    const[
+        const Route(path: '/heroes', name: 'Heroes', component: HeroesComponent)
+    ]
+)
+class AppComponent {
+    String title = 'Tour of heroes';
 }
-
