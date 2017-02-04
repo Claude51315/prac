14a6411720bcf9764a05f8677db35ab546ebcbc0
claude51315 <claude51315@gmail.com>
2017-02-04 10:33:33 +0800

    add FUNC_ATTR macro

--- raytracing/math-toolkit.h
+++ raytracing/math-toolkit.h
@@ -5,24 +5,29 @@
 #include <stdio.h>
 #include <assert.h>
 #include <immintrin.h>
-__attribute__((always_inline)) static inline
+
+#define FUNC_ATTR \
+    static inline \
+    __attribute__((always_inline))
+
+
+FUNC_ATTR
 void normalize(double *v)
 {
     double d = sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
-    assert(d != 0.0 && "Error calculating normal");
-
+    //assert(d != 0.0 && "Error calculating normal");
     v[0] /= d;
     v[1] /= d;
     v[2] /= d;
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 double length(const double *v)
 {
     return sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void add_vector(const double *a, const double *b, double *out)
 {
     //for (int i = 0; i < 3; i++)
@@ -33,7 +38,7 @@ void add_vector(const double *a, const double *b, double *out)
 
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void subtract_vector(const double *a, const double *b, double *out)
 {
     // original
@@ -56,7 +61,7 @@ void subtract_vector(const double *a, const double *b, double *out)
     out[2] = a[2] - b[2];
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void multiply_vectors(const double *a, const double *b, double *out)
 {
     //for (int i = 0; i < 3; i++)
@@ -67,7 +72,7 @@ void multiply_vectors(const double *a, const double *b, double *out)
     
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void multiply_vector(const double *a, double b, double *out)
 {
     //for (int i = 0; i < 3; i++)
@@ -77,7 +82,7 @@ void multiply_vector(const double *a, double b, double *out)
     out[2] = a[2] * b ;
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void cross_product(const double *v1, const double *v2, double *out)
 {
     out[0] = v1[1] * v2[2] - v1[2] * v2[1];
@@ -85,7 +90,7 @@ void cross_product(const double *v1, const double *v2, double *out)
     out[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 double dot_product(const double *v1, const double *v2)
 {
     //double dp = 0.0;
@@ -94,7 +99,7 @@ double dot_product(const double *v1, const double *v2)
     return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 void scalar_triple_product(const double *u, const double *v, const double *w,
                            double *out)
 {
@@ -102,7 +107,7 @@ void scalar_triple_product(const double *u, const double *v, const double *w,
     multiply_vectors(u, out, out);
 }
 
-__attribute__((always_inline)) static inline
+FUNC_ATTR
 double scalar_triple(const double *u, const double *v, const double *w)
 {
     double tmp[3];
