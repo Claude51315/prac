580749939040297596098b8117e1a2385fe5a44c
claude51315 <claude51315@gmail.com>
2017-02-04 09:26:50 +0800

    add loop unrolling
    current average execution time 2.24 seconds

--- raytracing/math-toolkit.h
+++ raytracing/math-toolkit.h
@@ -88,14 +88,10 @@ void cross_product(const double *v1, const double *v2, double *out)
 static inline
 double dot_product(const double *v1, const double *v2)
 {
-    double dp = 0.0;
+    //double dp = 0.0;
     //for (int i = 0; i < 3; i++)
     //    dp += v1[i] * v2[i];
-    dp += v1[0] * v2[0];
-    dp += v1[1] * v2[1];
-    dp += v1[2] * v2[2];
-    
-    return dp;
+    return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
 }
 
 static inline
