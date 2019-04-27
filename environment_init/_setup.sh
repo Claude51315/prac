d496fd235c5a1c30672689eb68777c65a2d31e31
claude51315 <claude51315@gmail.com>
2019-04-26 19:02:52 -0700

    add _setup.sh
    Signed-off-by: claude51315 <claude51315@gmail.com>

---
+++ environment_init/_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt-get install tmux vim git exuberant-ctags cscope
+git config --global user.name claude51315
+git config --global user.email claude51315@gmail.com
+git config --global core.editor vim
+ 
